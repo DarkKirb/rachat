6f2776ad97e36247e2405d9740a300d057c7717e
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-05-08 11:26:06 +0100

    initial commit

--- flake.nix
+++ flake.nix
@@ -19,8 +19,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix, ... } @ inputs: flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    cargo2nix,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
       overlays = [
         cargo2nix.overlays.default
         (import rust-overlay)
@@ -28,27 +35,135 @@
       pkgs = import nixpkgs {
         inherit system overlays;
       };
-      rustPkgs = pkgs.rustBuilder.makePackageSet' {
+      /*
+      qtMerged = with pkgs;
+      qt6.callPackage ({
+        env,
+        qtbase,
+      }:
+        env "qt-${qtbase.version}" (with qt6; [
+          qtwayland
+          libglvnd
+          qtdeclarative
+        ])) {};
+      */
+      qtMerged = pkgs.qt6.full;
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
         packageFun = import ./Cargo.nix;
-        rustChannel = "1.60.0";
-        packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+        rustChannel = "stable";
+        rustVersion = "latest";
+        packageOverrides = pkgs:
+          pkgs.rustBuilder.overrides.all
+          ++ [
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              name = "cxx-qt";
+              overrideAttrs = drv: {
+                dontWrapQtApps = true;
+                propagatedBuildInputs =
+                  drv.propagatedBuildInputs
+                  or []
+                  ++ [
+                    pkgs.qt6.qmake
+                  ];
+              };
+            })
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              name = "cxx-qt-lib";
+              overrideAttrs = drv: {
+                dontWrapQtApps = true;
+                postConfigure = let
+                  cxx_src = pkgs.rustBuilder.rustLib.fetchCratesIo {
+                    name = "cxx";
+                    version = "1.0.122";
+                    sha256 = "bb497fad022245b29c2a0351df572e2d67c1046bcef2260ebc022aec81efea82";
+                  };
+                in ''
+                  mkdir -pv include/rust
+                  mkdir no
+                  cd no
+                  tar -xvvf ${cxx_src}
+                  cd ..
+                  cp -rv no/*/include/* include/rust
+                  export CXXFLAGS="$CXXFLAGS -I$PWD/include"
+                '';
+                buildInputs =
+                  drv.buildInputs
+                  or []
+                  ++ [
+                    qtMerged
+                  ];
+                nativeBuildInputs =
+                  drv.nativeBuildInputs
+                  or []
+                  ++ [
+                    qtMerged
+                  ];
+                propagatedBuildInputs =
+                  drv.propagatedBuildInputs
+                  or []
+                  ++ [
+                    qtMerged
+                  ];
+              };
+            })
+            (pkgs.rustBuilder.rustLib.makeOverride {
+              name = "rachat";
+              overrideAttrs = drv: {
+                preConfigure = ''
+                  export PATH="${qtMerged}/bin:$PATH"
+                  export QMAKE="${qtMerged}/bin/qmake"
+                '';
+
+                buildInputs =
+                  drv.buildInputs
+                  or []
+                  ++ [
+                    qtMerged
+                  ];
+                nativeBuildInputs =
+                  drv.nativeBuildInputs
+                  or []
+                  ++ [
+                    qtMerged
+                  ];
+                propagatedBuildInputs =
+                  drv.propagatedBuildInputs
+                  or []
+                  ++ [
+                    qtMerged
+                    pkgs.qt6.wrapQtAppsHook
+                  ];
+                fixupPhase =
+                  drv.fixupPhase
+                  or ""
+                  + ''
+                    rm -rvvf $out
+                    mkdir $out
+                  '';
+              };
+            })
+          ];
       };
-    in
-    rec {
-      devShells.default = with pkgs; mkShell {
-        buildInputs = [
-          (rust-bin.nightly.latest.default.override {
-            extensions = [ "rust-src" ];
-          })
-          cargo2nix.packages.${system}.cargo2nix
-        ];
-      };
+    in rec {
+      devShells.default = with pkgs;
+        mkShell {
+          buildInputs = [
+            (rust-bin.nightly.latest.default.override {
+              extensions = ["rust-src"];
+            })
+            cargo2nix.packages.${system}.cargo2nix
+            alejandra
+            qt6.full
+            qtcreator
+          ];
+        };
       packages = {
-        chir-rs-crypto = rustPkgs.workspace.chir-rs-crypto { };
+        rachat = rustPkgs.workspace.rachat {};
       };
       nixosModules.default = import ./nixos {
         inherit inputs system;
       };
       hydraJobs = packages;
+      formatter = pkgs.alejandra;
     });
 }
