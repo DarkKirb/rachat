ee2d9d9dd7ceffe3224a10e7486fea79da803d46
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-06-08 12:40:31 +0100

    Fix windows path_to_bytes and add test

--- rachat-common/src/utils.rs
+++ rachat-common/src/utils.rs
@@ -16,6 +16,28 @@ pub fn path_to_bytes(path: impl AsRef<Path>) -> Vec<u8> {
     use safe_transmute::to_bytes::transmute_to_bytes;
     use std::os::windows::ffi::OsStrExt;
 
-    let path_buf = path.as_os_str().encode_wide().collect::<Vec<u8>>();
+    let path_buf = path.as_ref().as_os_str().encode_wide().collect::<Vec<u8>>();
     transmute_to_bytes(&path_buf[..]).to_vec()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(unix)]
+    #[test]
+    fn test_path_to_bytes() {
+        let path = Path::new("/foo/bar");
+        let bytes = path_to_bytes(path);
+        assert_eq!(bytes, b"/foo/bar");
+    }
+
+    #[cfg(windows)]
+    #[test]
+    fn test_path_to_bytes() {
+        let path = Path::new(r"C:\foo\bar");
+        let bytes = path_to_bytes(path);
+        assert_eq!(bytes, b"C\0:\0\\\0f\0o\0o\0\\\0b\0a\0r\0");
+    }
 }
